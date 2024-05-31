4ea0db02f4c10f99c49541df555db6a8f968ff44
Sherwin Zadeh <sherwin@artamata.com>
2024-05-31 14:14:53 -0700

    Added basing FirebaseAuth and FirebaseStoreage. For Auth, it includes signIn with email and password, create user with email and password, and ability to change your profile display name. Storage has the ability to get a StorageReference based on path and to get a downloadURL().

--- Sources/SkipFirebaseAuth/SkipFirebaseAuth.swift
+++ Sources/SkipFirebaseAuth/SkipFirebaseAuth.swift
@@ -2,27 +2,155 @@
 // under the terms of the GNU Lesser General Public License 3.0
 // as published by the Free Software Foundation https://fsf.org
 
+import SkipFoundation
 import SkipFirebaseCore
 #if SKIP
 import kotlinx.coroutines.tasks.await
+import android.net.Uri
 
 // https://firebase.google.com/docs/reference/swift/firebaseauth/api/reference/Classes/Auth
 // https://firebase.google.com/docs/reference/android/com/google/firebase/auth/FirebaseAuth
 
 public final class Auth {
-    public let auth: com.google.firebase.auth.FirebaseAuth
+    public let platformValue: com.google.firebase.auth.FirebaseAuth
 
-    public init(auth: com.google.firebase.auth.FirebaseAuth) {
-        self.auth = auth
+    public init(platformValue: com.google.firebase.auth.FirebaseAuth) {
+        self.platformValue = platformValue
     }
 
     public static func auth() -> Auth {
-        Auth(auth: com.google.firebase.auth.FirebaseAuth.getInstance())
+		Auth(platformValue: com.google.firebase.auth.FirebaseAuth.getInstance())
     }
 
     public static func auth(app: FirebaseApp) -> Auth {
-        Auth(auth: com.google.firebase.auth.FirebaseAuth.getInstance(app.app))
+		Auth(platformValue: com.google.firebase.auth.FirebaseAuth.getInstance(app.app))
     }
-}
-#endif
+	
+	public var app: FirebaseApp {
+		FirebaseApp(app: platformValue.getApp())
+	}
 
+	public var currentUser: User? {
+		guard let user = platformValue.currentUser else { return nil }
+		return User(user)
+	}
+	
+	public func signIn(withEmail email: String, password: String) async throws -> AuthDataResult {
+		// SKIP NOWARN
+		let result = try await platformValue.signInWithEmailAndPassword(email, password).await()
+		return AuthDataResult(result)
+	}
+	
+	public func createUser(withEmail email: String, password: String) async throws -> AuthDataResult {
+		// SKIP NOWARN
+		let result = try await platformValue.createUserWithEmailAndPassword(email, password).await()
+		return AuthDataResult(result)
+	}
+
+	public func signOut() throws {
+		try platformValue.signOut()
+	}
+}
+
+public class AuthDataResult: KotlinConverting<com.google.firebase.auth.AuthResult> {
+	public let platformValue: com.google.firebase.auth.AuthResult
+
+	public init(_ platformValue: com.google.firebase.auth.AuthResult) {
+		self.platformValue = platformValue
+	}
+	
+	public override func kotlin(nocopy: Bool = false) -> com.google.firebase.auth.AuthResult {
+		platformValue
+	}
+	
+	public var description: String {
+		platformValue.toString()
+	}
+	
+	public static func == (lhs: Self, rhs: Self) -> Bool {
+		lhs.platformValue == rhs.platformValue
+	}
+	
+	public var user: User {
+		User(platformValue.user!)
+	}
+}
+
+public class User: KotlinConverting<com.google.firebase.auth.FirebaseUser> {
+	public let platformValue: com.google.firebase.auth.FirebaseUser
+	
+	public init(_ platformValue: com.google.firebase.auth.FirebaseUser) {
+		self.platformValue = platformValue
+	}
+	
+	public override func kotlin(nocopy: Bool = false) -> com.google.firebase.auth.FirebaseUser {
+		platformValue
+	}
+	
+	public var description: String {
+		platformValue.toString()
+	}
+	
+	public static func == (lhs: Self, rhs: Self) -> Bool {
+		lhs.platformValue == rhs.platformValue
+	}
+	
+	public var isAnonymous: Bool {
+		platformValue.isAnonymous
+	}
+
+	public var providerID: String? {
+		platformValue.providerId
+	}
+
+	public var uid: String {
+		platformValue.uid
+	}
+
+	public var displayName: String? {
+		platformValue.displayName
+	}
+
+	public var photoURL: URL? {
+		guard let uri = platformValue.photoUrl else { return nil }
+		return URL(string: uri.toString())!
+	}
+
+	public var email: String? {
+		platformValue.email
+	}
+
+	public var phoneNumber: String? {
+		platformValue.phoneNumber
+	}
+	
+	public func createProfileChangeRequest() -> UserProfileChangeRequest {
+		return UserProfileChangeRequest(self)
+	}
+}
+
+public class UserProfileChangeRequest/*: KotlinConverting<com.google.firebase.auth.UserProfileChangeRequest>*/ {
+	var user: User
+	
+	fileprivate init(user: User) {
+		self.user = user
+	}
+	
+	public var displayName: String?
+	
+	public func commitChanges() async throws {
+		let builder = com.google.firebase.auth.UserProfileChangeRequest.Builder()
+		
+		if let displayName {
+			builder.setDisplayName(displayName)
+		}
+		
+		let platformChangeRequest: com.google.firebase.auth.UserProfileChangeRequest = builder.build()
+		
+		// SKIP NOWARN
+		try await user.platformValue.updateProfile(platformChangeRequest).await()
+	}
+}
+
+
+#endif
--- Sources/SkipFirebaseStorage/SkipFirebaseStorage.swift
+++ Sources/SkipFirebaseStorage/SkipFirebaseStorage.swift
@@ -2,33 +2,95 @@
 // under the terms of the GNU Lesser General Public License 3.0
 // as published by the Free Software Foundation https://fsf.org
 
+import SkipFoundation
 import SkipFirebaseCore
 #if SKIP
 import kotlinx.coroutines.tasks.await
+import android.net.Uri
 
 // https://firebase.google.com/docs/reference/swift/firebasestorage/api/reference/Classes/Storage
 // https://firebase.google.com/docs/reference/android/com/google/firebase/storage/FirebaseStorage
 
 public final class Storage {
-    public let storage: com.google.firebase.storage.FirebaseStorage
+    public let platformValue: com.google.firebase.storage.FirebaseStorage
 
-    public init(storage: com.google.firebase.storage.FirebaseStorage) {
-        self.storage = storage
+    public init(platformValue: com.google.firebase.storage.FirebaseStorage) {
+        self.platformValue = platformValue
     }
 
     public static func storage() -> Storage {
-        Storage(storage: com.google.firebase.storage.FirebaseStorage.getInstance())
+        Storage(com.google.firebase.storage.FirebaseStorage.getInstance())
     }
 
     public static func storage(app: FirebaseApp) -> Storage {
-        Storage(storage: com.google.firebase.storage.FirebaseStorage.getInstance(app.app))
+        Storage(com.google.firebase.storage.FirebaseStorage.getInstance(app.app))
     }
 
     public var app: FirebaseApp {
-        FirebaseApp(app: storage.getApp())
+        FirebaseApp(app: platformValue.getApp())
     }
 
+	public func reference() -> StorageReference {
+		return StorageReference(platformValue.reference)
+	}
 }
+
+public class StorageReference: KotlinConverting<com.google.firebase.storage.StorageReference> {
+	
+	public let platformValue: com.google.firebase.storage.StorageReference
+	
+	public init(platformValue: com.google.firebase.storage.StorageReference) {
+		self.platformValue = platformValue
+	}
+	
+	public override func kotlin(nocopy: Bool = false) -> com.google.firebase.storage.StorageReference {
+		platformValue
+	}
+	
+	public var description: String {
+		platformValue.toString()
+	}
+	
+	public static func == (lhs: Self, rhs: Self) -> Bool {
+		lhs.platformValue == rhs.platformValue
+	}
+	
+	public var storage: Storage {
+		Storage(platformValue.storage)
+	}
+	
+	public var bucket: String {
+		return platformValue.bucket
+	}
+	
+	public var fullPath: String {
+		return platformValue.path
+	}
+	
+	public var name: String {
+		return platformValue.name
+	}
+	
+	public func root() -> StorageReference {
+		return StorageReference(platformValue.root)
+	}
+	
+	public func parent() -> StorageReference? {
+		guard let parent = platformValue.parent else { return nil }
+		return StorageReference(parent)
+	}
+	
+	public func child(_ path: String) -> StorageReference {
+		return StorageReference(platformValue.child(path))
+	}
+	
+	public func downloadURL() async throws -> URL {
+		// SKIP NOWARN
+		let uri: android.net.Uri = try await platformValue.downloadUrl.await()
+		return URL(string: uri.toString())!
+	}
+}
+
 #endif
 
 //import FirebaseStorage
